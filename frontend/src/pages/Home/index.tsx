afb63df91cb6580ea1acd545332c47e488477a33
Ariane Mateus <ariane.ccomp@gmail.com>
2020-06-03 20:56:18 -0300

    feat: :lipstick: Create Point page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FiLogIn} from 'react-icons/fi'
+import {Link} from 'react-router-dom';
 
 import './styles.css'
 
@@ -17,7 +18,7 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</p>
 
-                    <a href="/cadastro"><span><FiLogIn /></span><strong>Cadastre um ponto de coleta.</strong></a>
+                    <Link to="/create-point"><span><FiLogIn /></span><strong>Cadastre um ponto de coleta.</strong></Link>
                 </main>
             </div>
         </div>
